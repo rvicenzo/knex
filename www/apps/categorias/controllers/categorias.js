801669c18cfc0dabb48f851e8f5038e619f4a603
Marcelo Dewes <portuga86@gmail.com>
2017-02-08 21:21:27 -0200

    Iniciando layout da pagina de categorias

--- www/apps/categorias/controllers/categorias.js
+++ www/apps/categorias/controllers/categorias.js
@@ -3,7 +3,27 @@ angular.module('starter.categorias', [])
 .controller('CategoriasCtrl', CategoriasCtrl);
 
 function CategoriasCtrl($scope) {
-
-  
-
+	$scope.trilhas = [
+		{
+			nome: 'Agência',
+			level: 'e',
+			percent: 90,
+			icone: 'agencia',
+			cursos: 4
+		},
+		{
+			nome: 'Agência',
+			level: 'e',
+			percent: 90,
+			icone: 'agencia',
+			cursos: 4
+		},
+		{
+			nome: 'Agência',
+			level: 'e',
+			percent: 90,
+			icone: 'agencia',
+			cursos: 4
+		}
+	];
 }
