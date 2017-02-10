0e5125258d29403a2a0b3823d01068df27aa78f6
rvicenzo <rafa.vicenzo@gmail.com>
2017-02-10 17:51:08 -0200

    tela atividade vr

--- www/js/app.js
+++ www/js/app.js
@@ -67,6 +67,15 @@ angular.module('starter', ['ionic', 'starter.controllers'])
         controller: 'PlaylistCtrl'
       }
     }
+  })
+
+  .state('app.vr', {
+    url: '/vr',
+    views: {
+      'menuContent': {
+        templateUrl: 'apps/vr/vr.html'
+      }
+    }
   });
   // if none of the above states are matched, use this as the fallback
   $urlRouterProvider.otherwise('/app/playlists');
