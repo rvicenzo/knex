059adb8e7ebf0dc41c8ea9aa724ceac1fd2b5dc9
Marcelo Dewes <portuga86@gmail.com>
2017-02-20 23:03:10 -0300

    Configurando array de eventos, função de prev e next do calendario e função de alteração de titulo da view

--- www/apps/calendario/controllers/calendario.js
+++ www/apps/calendario/controllers/calendario.js
@@ -3,4 +3,32 @@ angular.module('starter.calendario', [])
 .controller('CalendarioCtrl', CalendarioCtrl);
 
 function CalendarioCtrl($scope) {
+	$scope.eventos = [
+		{
+			startTime: new Date(2017, 1, 15, 15, 15),
+			endTime: new Date(2017, 1, 15, 16, 20),
+			title: 'Especial Automóveis VR',
+			allDay: false
+		},
+		{
+			startTime: new Date(2017, 1, 15, 20, 00),
+			endTime: new Date(2017, 1, 15, 21, 00),
+			title: 'Especial Automóveis VR Parte 2',
+			allDay: false
+		},
+		{
+			startTime: new Date(2017, 1, 20, 20, 00),
+			endTime: new Date(2017, 1, 20, 21, 00),
+			title: 'Evento de teste',
+			allDay: true
+		}
+	];
+
+	$scope.changeDate = function (action) {
+		$scope.$broadcast('changeDate', action);
+	}
+
+	$scope.onViewTitleChanged = function (title) {
+		$scope.viewTitle = title;
+	};
 }
